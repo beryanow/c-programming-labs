e57d9e5b7a4875264710321b8aae40734e4a82c8
Max Beryanow <beryanow@MacBook.local>
2017-11-22 20:41:24 +0700

    Adding_lab_6

---
+++ Lab_6 (Quick Sort)/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* qsort_implementation(int *arr, int size) {
+    int start = 0, end = size - 1;
+    int middle = arr[size / 2];
+    while (start <= end)  {
+            while (arr[start] < middle) {
+                start++;
+            }
+            while (arr[end] > middle) {
+                end--;
+            }
+            if (start <= end) {
+                int tmp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = tmp;
+                start++;
+                end--;
+            }
+    }
+
+    if (end > 0) {
+        qsort_implementation(arr, end + 1);
+    }
+
+    if (start < size) {
+        qsort_implementation(&arr[start], size - start);
+    }
+
+    return arr;
+}
+
+int main() {
+    int n, num;
+    scanf("%d", &n);
+    int* table = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num);
+        table[i] = num;
+    }
+    table = qsort_implementation(table, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", table[i]);
+    }
+    return 0;
+}
