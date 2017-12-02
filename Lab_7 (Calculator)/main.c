b6b9ed4bfe441062603551541c5b4daa6f805f9e
Max Beryanow <beryanow@MacBook.Dlink>
2017-12-02 22:16:56 +0700

    Quite_Finished_Calc

--- Lab_7 (Calculator)/main.c
+++ Lab_7 (Calculator)/main.c
@@ -2,14 +2,27 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     char *output = (char *) calloc(strlen(argv[1]), sizeof(char));
     char *stack = (char *) calloc(strlen(argv[1]), sizeof(char));
     int m = 0;
     int k = 0;
-    for (int i = 0; i < strlen(argv[1]); i++) {
+    int i = 0;
+
+    /* creating Reverse Polish notation */
+    while (i != strlen(argv[1])) {
         if ((argv[1][i] >= '0') && (argv[1][i] <= '9')) {
-            output[m] = argv[1][i];
+            int n = i;
+            while ((argv[1][i] >= '0') && (argv[1][i] <= '9')) {
+                i++;
+            }
+            output[m] = '(';
+            m++;
+            for (int z = n; z < i; z++) {
+                output[m] = argv[1][z];
+                m++;
+            }
+            output[m] = ')';
             m++;
         } else if ((argv[1][i] == '*') | (argv[1][i] == '/')) {
             while (1) {
@@ -24,6 +37,7 @@ int main(int argc, char* argv[]) {
                 }
                 k--;
             }
+            i++;
         } else if ((argv[1][i] == '+') | (argv[1][i] == '-')) {
             while (1) {
                 if ((stack[k - 1] == '*') | (stack[k - 1] == '/') | (stack[k - 1] == '+') | (stack[k - 1] == '-')) {
@@ -37,12 +51,12 @@ int main(int argc, char* argv[]) {
                 }
                 k--;
             }
-        }
-        else if (argv[1][i] == '(') {
+            i++;
+        } else if (argv[1][i] == '(') {
             stack[k] = argv[1][i];
             k++;
-        }
-        else if (argv[1][i] == ')') {
+            i++;
+        } else if (argv[1][i] == ')') {
             int c = k - 1, z;
             while (stack[k] != '(') k--;
             for (z = c; z > k; z--) {
@@ -52,7 +66,8 @@ int main(int argc, char* argv[]) {
             }
             stack[z] = '\0';
             k = z;
-        }
+            i++;
+        } else i++;
     }
     for (int i = k - 1; i >= 0; i--) {
         output[m] = stack[i];
@@ -60,38 +75,74 @@ int main(int argc, char* argv[]) {
         k--;
         m++;
     }
+    /* end of creating Reverse Polish notation */
 
-    /* start of calculating the result */
+    /* result calculation */
     int result = 0;
     k = 0;
-    while (strlen(output) != 1) {
+    int g;
+    int final_result;
+    while (output[strlen(output) - 1] != ')') {
         while (!((output[k] == '+') | (output[k] == '-') | (output[k] == '*') | (output[k] == '/'))) k++;
-        switch (output[k]) {
-            case '+': result = (output[k - 2] - '0') + (output[k - 1] - '0');
+        char f = output[k];
+        for (int x = k; x < strlen(output) - 1; x++) {
+            output[x] = output[x + 1];
+        }
+        output[strlen(output) - 1] = '\0';
+        k -= 2;
+        int result1 = 0, result2 = 0;
+        int h = 1;
+        while (output[k] != '(') {
+            result1 += (output[k] - '0') * h;
+            h *= 10;
+            for (int x = k; x < strlen(output) - 1; x++) {
+                output[x] = output[x + 1];
+            }
+            output[strlen(output) - 1] = '\0';
+            k--;
+        }
+        for (int x = k; x < strlen(output) - 2; x++) {
+            output[x] = output[x + 2];
+        }
+        output[strlen(output) - 1] = '\0';
+        output[strlen(output) - 1] = '\0';
+        k -= 2;
+        h = 1;
+        while (output[k] != '(') {
+            result2 += (output[k] - '0') * h;
+            h *= 10;
+            for (int x = k; x < strlen(output) - 1; x++) {
+                output[x] = output[x + 1];
+            }
+            output[strlen(output) - 1] = '\0';
+            k--;
+        }
+        switch (f) {
+            case '+':
+                result = result2 + result1;
                 break;
-            case '-': result = (output[k - 2] - '0') - (output[k - 1] - '0');
+            case '-':
+                result = result2 - result1;
                 break;
-            case '*': result = (output[k - 2] - '0') * (output[k - 1] - '0');
+            case '*':
+                result = result2 * result1;
                 break;
-            case '/': result = (int)((output[k - 2] - '0') / (output[k - 1] - '0'));
+            case '/':
+                result = result2 / result1;
                 break;
         }
-        output[k - 2] = (char)(result + '0');
-        for (int i = k - 1; i < strlen(output) - 2; i++) {
-            output[i] = output[i + 2];
+        final_result = result;
+        k++;
+         while (result != 0) {
+            for (int x = strlen(output); x > k; x--) {
+                output[x] = output[x - 1];
+            }
+            output[k] = (char) (result % 10 + '0');
+            result /= 10;
         }
-        int h = strlen(output);
-        for (int i = h - 1; i >= h - 2; i--) output[i] = '\0';
-        k = 0;
     }
-    printf("%d\n", result);
-    /* end of calculating the result */
- /*   for (int i = 0; i < m; i++) {
-        printf("%c ", output[i]);
-    }
-    printf("\n");
-    for (int i = 0; i < k; i++) {
-        printf("%c ", stack[i]);
-    } */
+    printf("%d\n", final_result);
+    /* end of result calculation */
+
     return 0;
 }
