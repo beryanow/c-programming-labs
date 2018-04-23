66272f28b508d658a0d1d49f5de63bded7d76175
beryanow <berjnov.ru@mail.ru>
2018-04-23 14:39:41 +0700

    Update

--- Lab_12 (Prim Algorithm)/main.c
+++ Lab_12 (Prim Algorithm)/main.c
@@ -1,116 +1,192 @@
 #include <stdio.h>
-#include <limits.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int validation_check (int N, int M) {
+typedef struct compose {
+    int length;
+    int start;
+    int end;
+    int val;
+} s_compose;
+
+void marking_as_read(int t, s_compose *arr, int *fin1, int y, int y2, int c) {
+    for (int v = 0; v <= t; v++) {
+        if (((arr[v].start == fin1[y]) && (arr[v].end == fin1[y2])) || (
+                (arr[v].start == fin1[y2]) && (arr[v].end == fin1[y])))
+            arr[v].val = c;
+    }
+}
+
+s_compose *sorting_edges(FILE *infile, int *t, int *mistake) {
+
+    int N, M;
+    fscanf(infile, "%d %d", &N, &M);
+    int start, end, len;
+
     if (!((N >= 0) && (N <= 5000))) {
-        printf("bad number of vertices");
-        return 0;
+        *mistake = 1;
+        printf("bad number of vertices\n");
     }
-    if (!((M >= 0) && (M <= N * (N - 1) / 2))) {
-        printf("bad number of edges");
-        return 0;
-    }
-    if (N - 1 > M) {
-        printf("no spanning tree");
-        return 0;
-    }
-    return 1;
-}
 
-int values_check (int start, int end, int length, int N) {
-    if (!((start >= 1) && (start <= N) && (end >= 1) && (end <= N))) {
-        printf("bad vertex");
-        return 0;
+    if (!((M >= 0) && (M <= N * (N + 1) / 2))) {
+        *mistake = 1;
+        printf("bad number of edges\n");
     }
-    if (!((length >= 0) && (length <= INT_MAX))) {
-        printf("bad length");
-        return 0;
-    }
-    return 1;
-}
 
-void find_min_spanning_tree_using_prim_algorithm(int vertices_amount, int* str_num, int* col_num, int* str_num1, int* col_num1, int* min, int** G, int* used_vertices) {
-    int finds_amount = 0;
-    while (finds_amount != vertices_amount - 1) {
-        printf("%d %d\n", *str_num + 1, *col_num + 1);
-        for (int i = 0; i < vertices_amount; i++) {
-            if (i != *col_num)
-                G[*str_num][i] = 0;
-            G[*col_num][i] = 0;
+    s_compose *arr = (s_compose *) malloc(sizeof(s_compose) * M);
+
+    for (int i = 2; i < M + 2; i++) {
+        start = -1;
+        end = -1;
+        len = -1;
+
+        fscanf(infile, "%d %d %d", &start, &end, &len);
+
+        if (start == -1) {
+            *mistake = 1;
+            printf("bad number of lines\n");
         }
-        *min = INT_MAX;
-        for (int i = 0; i < vertices_amount; i++) {
-            for (int j = 0; j < vertices_amount; j++) {
-                if ((G[i][j] > 0) && (G[i][j] <= *min) &&
-                        ((used_vertices[i] == 1) &&(used_vertices[j] == 0)) |
-                                ((used_vertices[i] == 0) &&(used_vertices[j] == 1)) && (((i != *str_num) | (j != *col_num)))) {
-                    *min = G[i][j];
-                    *str_num1 = i;
-                    *col_num1 = j;
+        if ((!((start >= 1) && (start <= N))) || (!((end >= 1) && (end <= N)))) {
+            *mistake = 1;
+            printf("bad vertex\n");
+        }
+
+        if (!((len >= 0) && (len <= INT_MAX))) {
+            *mistake = 1;
+            printf("bad length\n");
+        }
+
+        arr[*t].length = len;
+        arr[*t].start = start;
+        arr[*t].end = end;
+        arr[*t].val = 0;
+
+        (*t)++;
+    }
+
+    for (int i = 0; i < *t - 1; i++) {
+        for (int y = i + 1; y < *t; y++) {
+            if (arr[i].length > arr[y].length) {
+                s_compose temp = arr[i];
+                arr[i] = arr[y];
+                arr[y] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
+
+void checking_past_edges(int *fin, int *fin1, int *g, s_compose *arr, int t, int y, int y2) {
+    int u = 0;
+
+    for (int v = 0; v <= *g; v++) {
+        if (fin[v] == fin1[y2])
+            u = 1;
+    }
+
+    if (u == 0) {
+        (*g)++;
+        fin[*g] = fin1[y2];
+        marking_as_read(t, arr, fin1, y, y2, 1);
+    } else {
+        marking_as_read(t, arr, fin1, y, y2, -1);
+    }
+}
+
+void prim_alg_implementation(s_compose *arr, int t) {
+    int *fin = (int *) calloc(sizeof(int), t);
+    int *fin1 = (int *) calloc(sizeof(int), t);
+
+    int g = 0;
+    int g1 = 0;
+
+    for (int i = 0; i < t; i++) {
+        if (g == 0) {
+
+            fin[g] = arr[i].start;
+            g++;
+
+            fin[g] = arr[i].end;
+            arr[i].val = 1;
+
+        } else {
+            if (arr[i].val == 0) {
+
+                int h = 0;
+                int var1 = 0;
+
+                for (int k = 0; k <= g; k++) {
+                    if (arr[i].start == fin[k]) {
+                        h++;
+                        var1 = 1;
+                    } else if (arr[i].end == fin[k]) {
+                        h++;
+                    }
+                }
+
+                if (h == 1) {
+                    if (var1) {
+                        g++;
+                        fin[g] = arr[i].end;
+                    } else {
+                        g++;
+                        fin[g] = arr[i].start;
+                    }
+
+                    for (int y = 0; y <= g1; y++) {
+                        if ((fin1[y] == arr[i].start) || (fin1[y] == arr[i].end)) {
+
+                            if (y % 2 == 0) {
+                                checking_past_edges(fin, fin1, &g, arr, t, y, y + 1);
+                            } else {
+                                checking_past_edges(fin, fin1, &g, arr, t, y, y - 1);
+                            }
+
+                            for (int v = y; v < t - 2; v++) {
+                                fin1[v] = fin1[v + 2];
+                            }
+                            break;
+                        }
+                    }
+                    arr[i].val = 1;
+                } else if (h == 2) {
+                    arr[i].val = -1;
+                } else if (h == 0) {
+                    if (g1 == 0) {
+                        fin1[g1] = arr[i].start;
+                        g1++;
+                        fin1[g1] = arr[i].end;
+                    } else {
+                        g1++;
+                        fin1[g1] = arr[i].start;
+                        g1++;
+                        fin1[g1] = arr[i].end;
+                        g1--;
+                    }
                 }
             }
         }
-        *col_num = *col_num1;
-        *str_num = *str_num1;
-        used_vertices[*col_num] = 1;
-        used_vertices[*str_num] = 1;
-        finds_amount++;
     }
 }
 
-void find_min_graph_arc(int vertices_amount, int** graph_matrix, int* min, int* str_num, int* col_num, int* used_vertices) {
-    for (int i = 0; i < vertices_amount; i++) {
-        for (int j = 0; j < vertices_amount; j++) {
-            if ((graph_matrix[i][j] > 0) && (graph_matrix[i][j] < *min)) {
-                *min = graph_matrix[i][j];
-                *str_num = i;
-                *col_num = j;
-            }
-        }
-    }
-    used_vertices[*col_num] = 1;
-    used_vertices[*str_num] = 1;
-}
+int main(int argc, char *argv[]) {
+    FILE *infile = fopen(argv[1], "rb");
 
-int create_graph_matrix(int vertices_amount, int edges_amount, int **graph_matrix, int *used_vertices) {
-    for (int i = 0; i < vertices_amount; i++) {
-        graph_matrix[i] = (int *) malloc(vertices_amount * sizeof(int));
-    }
-    for (int i = 0; i < vertices_amount; i++) {
-        used_vertices[i] = 0;
-        for (int j = 0; j < vertices_amount; j++) {
-            graph_matrix[i][j] = 0;
-        }
-    }
-    for (int i = 0; i < edges_amount; i++) {
-        int start, end, length;
-        scanf("%d %d %d", &start, &end, &length);
-        if (values_check(start, end, length, vertices_amount) == 0) {
-            return -1;
-        }
-        graph_matrix[start - 1][end - 1] = length;
-        graph_matrix[end - 1][start - 1] = length;
+    int vert_amount = 0;
+    int mistake = 0;
+    s_compose *arr = sorting_edges(infile, &vert_amount, &mistake);
 
-    }
-    return 0;
-}
-
-int main() {
-    int N, M;
-    scanf("%d", &N);
-    scanf("%d", &M);
-    if (validation_check(N, M) == 0)
+    if (mistake)
         return 0;
 
-    int** graph_matrix = (int**) malloc(N * sizeof(int*));
-    int* used_vertices = (int*)malloc(N * sizeof(int));
-    if (create_graph_matrix(N, M, graph_matrix, used_vertices) == -1)
-        return 0;
+    prim_alg_implementation(arr, vert_amount);
 
-    int min = INT_MAX, str_num, col_num, str_num1, col_num1;
-    find_min_graph_arc(N, graph_matrix, &min, &str_num, &col_num, used_vertices);
-    find_min_spanning_tree_using_prim_algorithm(N, &str_num, &col_num, &str_num1, &col_num1, &min, graph_matrix, used_vertices);
-
+    if (vert_amount == 1)
+        printf("no spanning tree\n");
+    else
+        for (int i = 0; i < vert_amount; i++) {
+            if (arr[i].val == 1) printf("%d %d\n", arr[i].start, arr[i].end);
+        }
     return 0;
 }
